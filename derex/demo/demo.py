c42afcf4de97a2730c7f1a1e6b5d73f6f3faea71
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2019-08-06 18:30:38 +0200

    Changed plugin
    * Do not inherit from derex.runner BaseConfig class
    * Override only "openedx" services
    * Make "compose_path" a function instead of a method

--- derex/demo/demo.py
+++ derex/demo/demo.py
@@ -2,21 +2,20 @@ import logging
 import pkg_resources
 from typing import List, Dict, Callable
 
-from derex import runner
+from derex import runner  # type: ignore
 
 logger = logging.getLogger(__name__)
 
 
-class Config(runner.config.BaseConfig):
-    def compose_path(self, name: str) -> str:
-        return pkg_resources.resource_filename(__name__, f"compose/files/path/{name}")
+def compose_path(name: str) -> str:
+    return pkg_resources.resource_filename(__name__, f"compose/files/path/{name}")
 
+
+class Config:
     def yaml_opts_openedx(self) -> List[str]:
-        result = super(Config, self).yaml_opts_openedx()
-        result += ["-f", self.compose_path("demo.yml")]
-        return result
+        return ["-f", compose_path("demo.yml")]
 
     @runner.hookimpl
     def settings(self) -> Dict[str, Callable]:
         logger.info("Running in plugin Demo")
-        return {"services": self.yaml_opts_services, "openedx": self.yaml_opts_openedx}
+        return {"openedx": self.yaml_opts_openedx}
