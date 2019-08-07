0e5e26ec606f815c05f4e637b21826f3a4ffcfb2
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2019-08-07 15:20:56 +0200

    Added test

---
+++ tests/test_ddc.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from click.testing import CliRunner
+
+
+runner = CliRunner()
+
+
+def test_ensure_plugin_loaded():
+    from derex.runner.cli import ddc_ironwood
+
+    result = runner.invoke(ddc_ironwood, ["config"])
+
+    assert "Loading DemoConfig" in result.output
+    assert "demo_lms" in result.output
